83d3decc9fdab9842a7e8f35ee5599a26f1a1d8f
BharatiRuchika <ruchikabharaticdac@gmail.com>
2023-02-06 19:13:31 +0530

    desf

--- routes/users.js
+++ routes/users.js
@@ -3,9 +3,7 @@ var router = express.Router();
 var user = require("../module/user")
 var {isAuthenticatedUser} = require("../middlewares/auth")
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+
 router.post('/login', user.LoginUser);
 router.post('/register',user.registerUser );
 router.post('/sendMessage',isAuthenticatedUser,user.sendMessage);
